4a22170de68e440e825f6d7c1f14eae6a68e4014
JackTatt <wj.tattersall@mail.utoronto.ca>
2023-05-27 00:03:55 -0400

    Update connex_functions.py

--- connections/connex_functions.py
+++ connections/connex_functions.py
@@ -13,7 +13,27 @@ import pandas as pd
 import r5py
 import numpy as np
 
+# Inputs
+regionmap ##Path to OSM pbf
+gtfs # List of paths to GTFS zips
+settings # TNBuilderConfig
+gtha = r5py.TransportNetwork(regionmap,gtfs, settings)
+
+# Interchange Identification
+'''
+Search for main terminals/stations with common names
+Check list of interchange hubs made up of stations/terminals with muliple names 
+
+Collect list of all stops corresponding to each interchange
+
+'''
+
 def minimumCT(stop1, stop2):
     # Determine walking distance between stops/stations
     # R5py using 1m/s walking speed on OpenStreetMap
     
+def maximumCT(stop1,stop2):
+    # Define based on a fixed value
+    # Define based on multiple of headway. 
+    
+# Perform analysis
